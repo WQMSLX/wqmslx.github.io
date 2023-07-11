fd4f4f74a18ab5d010635e30b343f53572dac7ff
wangqiming <1358632259@qq.com>
2023-07-11 15:21:57 +0800

    关闭评论插件

--- src/.vuepress/plugins.ts
+++ src/.vuepress/plugins.ts
@@ -1,4 +1,10 @@
 import search from "./search.js"
+import { autoCatalogPlugin } from "vuepress-plugin-auto-catalog";
+
+const base = <"/" | `/${string}/`>process.env["BASE"] || "/";
 export default ([
-    search
+    search,
+    autoCatalogPlugin({
+      //插件选项
+    }),
 ])
--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -85,11 +85,12 @@ export default hopeTheme({
   plugins: {
     blog: true,
 
-    comment: {
-      // You should generate and use your own comment service
-      provider: "Waline",
-      serverURL: "https://waline-comment.vuejs.press",
-    },
+    comment: false,
+    // comment: {
+    //   // You should generate and use your own comment service
+    //   provider: "Waline",
+    //   serverURL: "https://waline-comment.vuejs.press",
+    // },
 
     // all features are enabled for demo, only preserve features you need here
     mdEnhance: {
